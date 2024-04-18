b4b15340cbb48d727c7319612280c6f505ab5b3b
tapiwamak <tapiwa.makamure@stewardbank.co.zw>
2024-04-19 01:05:37 +0200

    changed newest selector

--- tasks.py
+++ tasks.py
@@ -1,6 +1,4 @@
 
-
-from selenium.webdriver.support.ui import Select
 from selenium import webdriver 
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -13,10 +11,8 @@ from urllib.parse import urlparse, parse_qs
 import requests
 import time
 import logging
-from selenium.common.exceptions import TimeoutException,NoSuchElementException, WebDriverException
-import re, os
+import re, os, json
 import pandas as pd
-import json
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
@@ -24,39 +20,23 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
-service = Service(executable_path="/usr/bin/chromedriver")
-driver = webdriver.Chrome(service=service, options=options)
 
-# service = Service(executable_path="chromedriver.exe")
-# driver = webdriver.Chrome(service=service)
+# Use the pre-installed ChromeDriver in the Docker image
+service = Service(executable_path="/usr/bin/chromedriver")
+
+driver = webdriver.Chrome(service=service, options=options)
 
 def read_config(configfile):
     with open(configfile, 'r') as config_file:
         data = json.load(config_file)
     return data
 
-
-def count_occurrences(string, search_phrase):
-    return string.lower().count(search_phrase.lower())
-
-def contains_money(string):
-    # Define possible formats for money
-    money_formats = [
-        r"\$\d+(\.\d{1,2})?",  # $11.1 or $111,111.11
-        r"\d+ dollars",  # 11 dollars
-        r"\d+ USD"  # 11 USD
-    ]
-    
-    # Check if the string contains any of the money formats
-    for money_format in money_formats:
-        if re.search(money_format, string, re.IGNORECASE):
-            return True
-
-    return False
-
+# service = Service(executable_path="chromedriver.exe")
+# driver = webdriver.Chrome(service=service)
 
 
 logging.info("Started")
+
 
 folder_Download = r"output"
 if not os.path.exists(folder_Download):
@@ -79,21 +59,10 @@ def wait_for_element(driver, locator, timeout=10):
 
 search_button.click()
 
-try:
-    input_element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[data-element='search-form-input']")))
-    input_element.send_keys(search_phrase+Keys.ENTER)
-    print("Done")
-except TimeoutException:
-    print("Timeout: The input element is not clickable within the specified timeout period.")
-except NoSuchElementException:
-    print("Element not found: The input element does not exist on the page.")
-except WebDriverException as e:
-    logging.error("Crashed "+e)
-
-# print("Clicked Search")
-# input_element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[data-element='search-form-input']")))
-# input_element.send_keys(search_phrase+Keys.ENTER)
-# print("Done")
+print("Clicked Search")
+input_element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[data-element='search-form-input']")))
+input_element.send_keys(search_phrase+Keys.ENTER)
+print("Done")
 
 select_element = driver.find_element(By.CLASS_NAME, "select-input")
 select_element.click()
@@ -101,29 +70,31 @@ select_element.click()
 newest_option = driver.find_element(By.XPATH, "//option[@value='1']")  # Assuming '1' is for "Newest"
 newest_option.click()
 
+# span_element = wait.until(EC.presence_of_element_located((By.XPATH, "//span[text()='Entertainment & Arts']")))
+# checkbox = span_element.find_element(By.XPATH, "./preceding-sibling::input[@type='checkbox']")
+# checkbox.click()
 
-#time.sleep(100)
+time.sleep(10)
 
 articles = driver.find_elements(By.XPATH, "//ps-promo")
 print(f"Found {len(articles)} articles.")
 logging.info(f"Found {len(articles)} articles.")
 
-search_phrase_counts = []
-contains_money_list = []
+
+# Define a function to check for money formats
+def contains_money(text):
+    return False
 
 # Extract data for each article
 for article in articles:
     # Extract title
     article_data = {}
-    
-    try:
-        title_element = article.find_element(By.XPATH, ".//h3[@class='promo-title']/a")
-        title = title_element.text.strip()        
-        print("Title:", title)
-    except Exception as e:
-        title = "Title not found"
+    title_element = article.find_element(By.XPATH, ".//h3[@class='promo-title']/a")
+    title = title_element.text.strip()
     article_data['Title'] = title
-
+    print("Title:", title)
+    
+    # Extract date
     try:
         date_element = article.find_element(By.XPATH, ".//p[@class='promo-timestamp']")
         date = date_element.text.strip()
@@ -143,23 +114,6 @@ for article in articles:
 
     article_data['Description'] = description
 
-    try:
-        count = count_occurrences(title, search_phrase) + count_occurrences(description, search_phrase)
-        
-    except Exception as e:
-        count = 0
-    article_data['Search Phrase Count'] = count   
-
-    
-    # Check if e title / description contains any amount of money
-    try:
-        contains_money_flag = contains_money(title) or contains_money(description)
-        
-    except Exception as e:
-        contains_money_flag = False
-    article_data['Contains Money'] = contains_money_flag   
-
-
     # Get the description
     try:
         picture_element = article.find_element(By.XPATH, ".//img[@class='image']")
@@ -168,7 +122,7 @@ for article in articles:
         picture_src = picture_element.get_attribute("src")
         print("Picture SRC:", picture_src)
         print("Picture description:>>>>>> ",picture_description)
-        article_data['Picture Description'] = picture_description
+        article_data['File Name Description'] = picture_description
         parsed_url = urlparse(picture_src)
 
         picture_filename_download = parsed_url.path.split("/")[-1]
@@ -179,7 +133,7 @@ for article in articles:
             print(">>>>>>>>>>>> Response is >>>>>>>>>>>>>>"+str(response.status_code))
             print("Try to download image")
 # Check if the request was successful
-            
+             
             query_params = parse_qs(parsed_url.query)
 
             if 'url' in query_params:
@@ -201,7 +155,7 @@ for article in articles:
                         print("Image downloaded successfully as:", output_file_path)
                     else:
                         print("Failed to download image." +str(response.status_code))
-    
+       
                 else:
                     picture_filename = "Filename not found"
             else:
@@ -215,7 +169,7 @@ for article in articles:
         except Exception as e:
             print("Error is >>>>>>> {}".format(e))
     except Exception as e:
-        picture_description = "Picture not available"  
+        picture_description = "Picture not available"    
     
 
     articles_data.append(article_data)
@@ -223,10 +177,30 @@ for article in articles:
 
 df = pd.DataFrame(articles_data)
 
-# Write DFrame to file
+# Write DataFrame to an Excel file
 excel_file = os.path.join(folder_Download, "articles_data.xlsx")
 df.to_excel(excel_file, index=False)
 
+        
+
+# Parse the URL
+
+
+# Get the filename from the path
+
+
+    # Get the picture URL
+    # picture_url = picture_element.get_attribute("src")
+
+    # print("Picture Description:", picture_description)
+    # print("Picture URL:", picture_url)
+    
+    # Log the extracted data
+    # print("Title:", title)
+    # print("Date Published:", date)
+    # print("Description:", description)
+    #print("Picture URL:", picture_url)    
+
+
+# Close the browser
 driver.quit()
-
-
