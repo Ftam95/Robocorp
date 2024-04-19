a296d9587da42d399d0e6577776fa2b74fbb7490
tapiwamak <tapiwa.makamure@stewardbank.co.zw>
2024-04-19 12:14:08 +0200

    update logging

--- tasks.py
+++ tasks.py
@@ -20,8 +20,16 @@ class LaTimes:
         self.articles_data = []
 
         # Setting up the Selenium WebDriver
-        self.service = Service(executable_path="chromedriver.exe")
-        self.driver = webdriver.Chrome(service=self.service)
+
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+
+        # # Use the pre-installed ChromeDriver in the Docker image
+        service = Service(executable_path="/usr/bin/chromedriver")
+        self.driver = webdriver.Chrome(service=service, options=options)
+        # self.service = Service(executable_path="chromedriver.exe")
+        # self.driver = webdriver.Chrome(service=self.service)
         self.wait = WebDriverWait(self.driver, 10)  # Wait up to 10 seconds
 
     def read_config(self, configfile):
