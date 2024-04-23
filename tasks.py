7975572d5230c898086ca0d1240df5f81ac080ab
tapiwamak <tapiwa.makamure@stewardbank.co.zw>
2024-04-23 14:34:38 +0200

    configuration file

--- tasks.py
+++ tasks.py
@@ -24,15 +24,15 @@ from RPA.Robocorp.WorkItems import WorkItems
 
 @task
 def otomatika():
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
+    # options = Options()
+    # options.add_argument("--headless")
+    # options.add_argument("--no-sandbox")
 
 #     # # # # Use the pre-installed ChromeDriver in the Docker image
-    service = Service(executable_path="/usr/bin/chromedriver")
-    driver = webdriver.Chrome(service=service, options=options)
-    #service = Service(executable_path="chromedriver.exe")
-    #driver = webdriver.Chrome(service=service)
+    # service = Service(executable_path="/usr/bin/chromedriver")
+    # driver = webdriver.Chrome(service=service, options=options)
+    service = Service(executable_path="chromedriver.exe")
+    driver = webdriver.Chrome(service=service)
 
     logging.info("Started")
 
