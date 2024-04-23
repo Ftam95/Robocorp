e31be37e8f19a7ec99e3050fea01e891757d2dff
tapiwamak <tapiwa.makamure@stewardbank.co.zw>
2024-04-23 16:26:59 +0200

    config file

--- config_reader.py
+++ config_reader.py
@@ -1,25 +1,25 @@
 import json
 
-def read_config(configfile):
-     with open(configfile, 'r') as config_file:
-         data = json.load(config_file)
-     return data
+# def read_config(configfile):
+#      with open(configfile, 'r') as config_file:
+#          data = json.load(config_file)
+#      return data
 
 
-# from RPA.Robocorp.WorkItems import WorkItems
+from RPA.Robocorp.WorkItems import WorkItems
 
-# # Initialize the Work Items library
+# Initialize the Work Items library
 
-# def read_config():
-#     # Initialize the Work Items library
-#     workitems = WorkItems()
-#     print("work items >>>>")
+def read_config():
+    # Initialize the Work Items library
+    workitems = WorkItems()
+    print("work items >>>>")
 
-#     # Load the current Work Item
-#     work_item = workitems.get_work_item_payload()
-#     print("pay load >>>>>>>>>>",work_item)
+    # Load the current Work Item
+    work_item = workitems.get_work_item_payload()
+    print("pay load >>>>>>>>>>",work_item)
 
-#     return work_item
+    return work_item
 
 
 
